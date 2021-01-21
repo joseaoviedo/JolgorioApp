bf6d33824a88c795e9ce3adf3d0ec69fc9bb5f6d
Alejandro Alfaro Vargas <55007887+ale64gt@users.noreply.github.com>
2021-01-20 18:49:20 -0600

    contactos top fix

--- app/src/main/java/com/jolgorio/jolgorioapp/ui/videocall/Activity_contacts.java
+++ app/src/main/java/com/jolgorio/jolgorioapp/ui/videocall/Activity_contacts.java
@@ -13,11 +13,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 import com.jolgorio.jolgorioapp.R;
 import com.jolgorio.jolgorioapp.ui.main.SectionsPagerAdapter;
 
 public class Activity_contacts extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
