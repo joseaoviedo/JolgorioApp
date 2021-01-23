fb21be9fed0e3cf1994aa8fb2634bae6ab60d0ab
rvenegas <risaacvene24@gmail.com>
2021-01-23 14:37:55 -0600

    SelectGenderButtons

--- app/src/main/java/com/jolgorio/jolgorioapp/ui/register/RegisterFragment.java
+++ app/src/main/java/com/jolgorio/jolgorioapp/ui/register/RegisterFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.widget.AppCompatButton;
@@ -16,6 +17,9 @@ import com.jolgorio.jolgorioapp.R;
 
 public class RegisterFragment extends Fragment {
     private Button leavingAlertButton;
+    private Button emergencyCallButton;
+    private ImageButton maleButton;
+    private ImageButton femaleButton;
     private AlertDialog dialog;
     private NavController navController;
 
@@ -26,7 +30,22 @@ public class RegisterFragment extends Fragment {
         NavHostFragment navHostFragment = (NavHostFragment) getActivity().getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
         navController = navHostFragment.getNavController();
         LeavingAlert(view);
+        EmergencyCallAlert(view);
+        GenderSelection(view);
         return view;
+    }
+
+    private void GenderSelection(View view) {
+        maleButton = (ImageButton) view.findViewById(R.id.MaleButton);
+        femaleButton = (ImageButton) view.findViewById(R.id.FemaleButton);
+
+        maleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 
     private void LeavingAlert(View view){
@@ -37,6 +56,43 @@ public class RegisterFragment extends Fragment {
             public void onClick(View v) {
                 AlertDialog.Builder alerta = new AlertDialog.Builder(getActivity());
                 final View popupView = getLayoutInflater().inflate(R.layout.fragment_leaving_register_alert, null);
+
+                AppCompatButton exitBtn = popupView.findViewById(R.id.alertYESbutton);
+                AppCompatButton cancelBtn = popupView.findViewById(R.id.alertNObutton);
+
+                alerta.setView(popupView);
+                dialog = alerta.create();
+                dialog.show();
+
+                if(exitBtn != null) {
+                    exitBtn.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            dialog.dismiss();
+                            navController.navigate(R.id.action_registerFragment_to_mainMenuFragment);
+                        }
+                    });
+                }
+                if(cancelBtn != null) {
+                    cancelBtn.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            dialog.dismiss();
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    private void EmergencyCallAlert(View view){
+        emergencyCallButton = (Button) view.findViewById(R.id.EmergencyButton);
+
+        emergencyCallButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder alerta = new AlertDialog.Builder(getActivity());
+                final View popupView = getLayoutInflater().inflate(R.layout.layout_emergencycall_popup, null);
 
                 AppCompatButton exitBtn = popupView.findViewById(R.id.alertYESbutton);
                 AppCompatButton cancelBtn = popupView.findViewById(R.id.alertNObutton);
