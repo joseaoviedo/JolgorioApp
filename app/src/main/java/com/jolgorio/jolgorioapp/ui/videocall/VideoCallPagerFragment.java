20acff2b1e4873c850b81e20105d5dd335da4bc2
joseaoviedo <marucho147@gmail.com>
2021-01-22 23:30:13 -0600

    Fix a los títulos del directorio de llamadas

--- app/src/main/java/com/jolgorio/jolgorioapp/ui/videocall/VideoCallPagerFragment.java
+++ app/src/main/java/com/jolgorio/jolgorioapp/ui/videocall/VideoCallPagerFragment.java
@@ -16,20 +16,15 @@ import java.util.ArrayList;
 
 
 public class VideoCallPagerFragment extends Fragment {
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_videocall_pager, container, false);
         TabLayout tabLayout = view.findViewById(R.id.videocall_tab_layout);
         ViewPager viewPager = view.findViewById(R.id.videocall_viewpager);
-        ArrayList<String> arrayList = new ArrayList<>();
         VideoCallPagerAdapter adapter = new VideoCallPagerAdapter(getChildFragmentManager());
         viewPager.setAdapter(adapter);
         viewPager.setOffscreenPageLimit(3);
         tabLayout.setupWithViewPager(viewPager, true);
         return view;
     }
-
 }
