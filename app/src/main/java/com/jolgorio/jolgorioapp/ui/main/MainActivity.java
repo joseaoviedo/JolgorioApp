50918dcfd67f23229c6dceaa36214f1865dfefa3
rvenegas <risaacvene24@gmail.com>
2021-01-23 23:02:08 -0600

    Registro funcional
    Aún falta tomar fotos con cámara y de la galería, demás de la llamada de emergencia.
    
    Parece ser que se úede tomar fotos desde los fragments en esta versión de android studio.

--- app/src/main/java/com/jolgorio/jolgorioapp/ui/main/MainActivity.java
+++ app/src/main/java/com/jolgorio/jolgorioapp/ui/main/MainActivity.java
@@ -1,17 +1,33 @@
 package com.jolgorio.jolgorioapp.ui.main;
 
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.provider.MediaStore;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.jolgorio.jolgorioapp.R;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
     @Override
     public void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Locale locale = new Locale("es_ES");
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.locale = locale;
+        getApplicationContext().getResources().updateConfiguration(config, null);
     }
+
 }
