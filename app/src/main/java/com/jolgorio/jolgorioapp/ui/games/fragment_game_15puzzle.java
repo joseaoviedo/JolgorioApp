fb941ed3d9fa37f7a63f7fcd6b0a3513b28e3c9e
Alejandro Alfaro Vargas <55007887+ale64gt@users.noreply.github.com>
2021-01-29 18:43:58 -0600

    juego

--- app/src/main/java/com/jolgorio/jolgorioapp/ui/games/fragment_game_15puzzle.java
+++ app/src/main/java/com/jolgorio/jolgorioapp/ui/games/fragment_game_15puzzle.java
@@ -1,12 +1,15 @@
 package com.jolgorio.jolgorioapp.ui.games;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -32,9 +35,10 @@ public class fragment_game_15puzzle extends Fragment implements View.OnClickList
     private int[] tiles;
     private TextView textSteps;
     private int stepCount = 0;
-    private TextView textTime;
-    private Timer timer;
-    private int timeCount = 0;
+    private Button reiniciar;
+    private Button emergencia;
+    private AlertDialog.Builder alertDialogBuilder;
+    private AlertDialog dialog;
 
     View view;
 
@@ -100,48 +104,55 @@ public class fragment_game_15puzzle extends Fragment implements View.OnClickList
     private void loadViews() {
         group = view.findViewById(R.id.group);
         textSteps = view.findViewById(R.id.steps);
-        textTime = view.findViewById(R.id.time);
+        reiniciar = view.findViewById(R.id.revolver);
+        emergencia = view.findViewById(R.id.EmergencyButton);
+
         buttons = new Button[4][4];
 
         for(int i = 0; i< group.getChildCount(); i++){
             buttons[i/4][i%4] = (Button) group.getChildAt(i);
             buttons[i/4][i%4].setOnClickListener(this);
         }
-    }
 
-    private void loadTimer() {
-        timer = new Timer();
-        timer.schedule(new TimerTask() {
+        reiniciar.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                getActivity().runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        timeCount++;
-                        setTime(timeCount);
-                    }
-                });
+            public void onClick(View v) {
+                generateNumbers();
+                loadDataToViews();
+                stepCount = 0;
+                textSteps.setText("Pasos: "+stepCount);
             }
-        },1000,1000);
+        });
+
+        emergencia.setOnClickListener((View.OnClickListener) this);
     }
 
-    private void setTime(int timeCount){
-        int second = timeCount % 60;
-        int hour = timeCount / 3600;
-        int minute = (timeCount - hour*3600)/60;
-        textTime.setText(String.format("Time: %02d:%02d",minute,second));
-    }
 
     @Override
     public void onClick(View v) {
-        buttonClick(v);
-    }
 
+        if (v.getId() == R.id.EmergencyButton) {
+            EmergencyCall call = new EmergencyCall();
+            call.EmergencyPopUp(this);
+
+        }else if (v.getId() == R.id.jugarotravez) {
+            dialog.dismiss();
+            generateNumbers();
+            loadDataToViews();
+            stepCount = 0;
+            textSteps.setText("Pasos: "+stepCount);
+        }else if (v.getId() == R.id.otrojuego) {
+            //Regresar al menu de juegos
+
+        }else if (v.getId() == R.id.salir) {
+            //Regresar al menu principal
+        }else {
+            buttonClick(v);
+        }
+    }
     public void buttonClick(View v){
         Button button = (Button) v;
-        if(stepCount == 0){
-            loadTimer();
-        }
+
         int x = button.getTag().toString().charAt(0) - '0';
         int y = button.getTag().toString().charAt(1) - '0';
 
@@ -173,13 +184,36 @@ public class fragment_game_15puzzle extends Fragment implements View.OnClickList
 
         }
         if(isWin){
-            Toast.makeText(view.getContext(), "Gano!", Toast.LENGTH_SHORT).show();
+            finish();
             for(int i = 0; i < group.getChildCount(); i++){
                 buttons[i/4][i%4].setClickable(false);
             }
-            timer.cancel();
+            reiniciar.setClickable(false);
         }
 
+    }
+
+    public void finish(){
+        Log.d("3", "Juego Terminado");
+        alertDialogBuilder = new AlertDialog.Builder(getActivity());
+        final View imageDisplay = getLayoutInflater().inflate(R.layout.layout_game_win_popup, null);
+        AppCompatButton jugarotravez = imageDisplay.findViewById(R.id.jugarotravez);
+        AppCompatButton otrojuego = imageDisplay.findViewById(R.id.otrojuego);
+        AppCompatButton salir = imageDisplay.findViewById(R.id.salir);
+        if(jugarotravez != null) {
+            jugarotravez.setOnClickListener((View.OnClickListener) this);
+        }
+
+        if(otrojuego != null) {
+            otrojuego.setOnClickListener((View.OnClickListener) this);
+        }
+
+        if(salir != null) {
+            salir.setOnClickListener((View.OnClickListener) this);
+        }
+        alertDialogBuilder.setView(imageDisplay);
+        dialog = alertDialogBuilder.create();
+        dialog.show();
     }
 
 }
